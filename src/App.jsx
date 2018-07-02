895deca5325702181ea6284518261327e168c074
Test User <danielway91@live.com>
2018-07-01 18:14:40 -0700

    Inital commit of Music Master React Tutorial

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import './App.css'; // Lets you use CSS in App directly
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: 'Snyper_Wolf',
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state', this.state);
+    }
+    render() {
+        return (
+            <div className="App">
+                <div className='App-title'>{this.state.title}</div>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            type="text" placeholder="Search for an Artist"
+                            value={this.state.query}
+                            onChange={event => {this.setState({ query: event.target.value})}}
+                            onKeyPress={event => {
+                                if(event.key === 'Enter') {
+                                    this.search()
+                                }
+                            }}/>
+                        <InputGroup.Addon onClick={() => this.search()}>
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+                <div placeholder="Artist Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+                <div className="Gallery">
+                    Gallery
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
